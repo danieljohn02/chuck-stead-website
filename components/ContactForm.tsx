24d3c57523b8b3f3f7a051b95e0ef7c0869868b0
danieljohn02 <danieljohn02@icloud.com>
2026-05-28 21:04:18 -0400

    Fix Netlify Forms: POST to /__forms.html instead of / so Next.js doesn't intercept

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -24,7 +24,7 @@ export default function ContactForm() {
     formData.forEach((v, k) => { payload[k] = v.toString(); });
 
     try {
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode(payload),
